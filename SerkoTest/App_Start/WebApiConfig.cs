281b198624c874e74ae79422cae88f3b334f27a5
DESKTOP-89QQA22\shank <sneha.shankur@genesisenergy.co.nz>
2018-10-27 00:39:14 +1300

    Initial project set up along with Extract XML data from text and Calculate GST

---
+++ SerkoTest/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using SerkoTest.Interface;
+using SerkoTest.Services;
+using Ninject.Modules;
+using Ninject;
+using System.Reflection;
+using SerkoTest.Interface;
+using SerkoTest.Controllers;
+using Unity;
+using Unity.Lifetime;
+
+namespace SerkoTest
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
